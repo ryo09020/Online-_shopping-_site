576ba59397e27797e75c43d9b83327846ecc77e5
EC2 Default User <ec2-user@ip-172-31-8-36.ap-northeast-1.compute.internal>
2023-07-01 10:18:45 +0000

    change_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 管理者情報の登録
-admin = Admin.create(email: 'b@b', password: 'bbbbbb')
+# Admin.create!(
+#   email: 'c@c',
+#   password: 'cccccc'
+# )
 
 # ジャンル情報の登録
-genre1 = Genre.create(name: 'ケーキ')
-genre2 = Genre.create(name: 'パン')
+# genre1 = Genre.create!(name: 'ケーキ')
+# genre2 = Genre.create!(name: 'パン')
 
 # 商品情報の登録
-item1 = Item.create(name: 'いちごのショートケーキ', genre_id: genre1.id, price_no_tax: 1000, body: 'itigo')
-# item2 = Item.create(image: 'images/no_image.jpg',name: 'クリームパン', genre_id: genre2.id, price_no_tax: 2000,body: 'opjpo')
+item1 = Item.create!(
+  name: 'いちごのショートケーキ',
+  genre_id: 6,
+  price_no_tax: 1000,
+  body: 'itigo',
+  sales_status: true
+)
+item1.image.attach(io: File.open('./app/assets/images/no_image.jpg'), filename: 'no_image.jpg')
+
+item2 = Item.create!(
+  name: 'クリームパン',
+  genre_id: 7,
+  price_no_tax: 2000,
+  body: 'opjpo',
+  sales_status: true
+)
+item2.image.attach(io: File.open('./app/assets/images/no_image.jpg'), filename: 'no_image.jpg')
