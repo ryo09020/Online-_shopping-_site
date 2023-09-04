1c13230162396861f3bad2e1279d0e75dcd0e1f8
ryo09020 <ryozinl924@gmail.com>
2023-09-04 04:28:36 +0000

    8_channel

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,6 +14,7 @@ class Admin::OrdersController < ApplicationController
       if order.status== "paid"
         order.order_details.update_all(making_status: 1)
       end
+      binding.pryorder
       redirect_to admin_order_path(order.id)
     end
   end
